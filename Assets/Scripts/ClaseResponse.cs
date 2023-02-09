904548919c7de3f6148ffada6f8c282472f9df23
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-02-09 21:50:49 +0100

    Comentarios explicativos añadidos

--- Assets/Scripts/ClaseResponse.cs
+++ Assets/Scripts/ClaseResponse.cs
@@ -3,5 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClaseResponse {
+    //Creamos una Lista de resultados para usar los datos serializados
     public List<ClaseQuestion> results;
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,13 @@ using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour {
    void Start() {
-        // A correct website page.
+        //Insertamos la URL de la API
         StartCoroutine(GetRequest("https://opentdb.com/api.php?amount=10"));
     }
 
     IEnumerator GetRequest(string uri) {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
-            // Request and wait for the desired page.
+            //Solicitamos y esperamos a la página en cuestion
             yield return webRequest.SendWebRequest();
 
             string[] pages = uri.Split('/');
@@ -26,19 +26,28 @@ public class GameManager : MonoBehaviour {
                     Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
                     break;
                 case UnityWebRequest.Result.Success:
+                    //Este es el caso que necesitamos y nos interesa, creamos una función para inicializar
+                    //en la que enviaremos los datos serializados de la API
                     ParserJson(webRequest.downloadHandler.text);
                     break;
             }
         }
     }
 
+    //Función con la que imprimiremos los datos solicitados
     private void ParserJson(string Json) {
         ClaseResponse claseResponse = JsonUtility.FromJson<ClaseResponse>(Json);
-        
+
+        //Imprimimos en la consola de Unity el número de preguntas que recibimos de la API
         Debug.Log("Nº de preguntas: " + claseResponse.results.Count);
+        //Ahora imprimimos cuatro de los datos de una de las preguntas (la primera)
+        //1º La Categoría
         Debug.Log("Caterogía: " + claseResponse.results[0].category);
+        //2º El Tipo
         Debug.Log("Tipo de pregunta: " + claseResponse.results[1].type);
+        //3º La Dificultad
         Debug.Log("Dificultad: " + claseResponse.results[2].difficulty);
+        //4º La Pregunta
         Debug.Log("Pregunta: " + claseResponse.results[3].question);
     }
 }
